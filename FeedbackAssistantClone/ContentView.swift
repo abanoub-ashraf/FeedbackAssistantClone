f0a78dc564bc1d6a8a43d4f78e413280f344b1e7
Abanoub Ashraf <abanoub.ashraf10@gmail.com>
2024-02-25 00:07:39 +0200

    fill content view with data from selected filter

--- FeedbackAssistantClone/ContentView.swift
+++ FeedbackAssistantClone/ContentView.swift
@@ -8,11 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Content")
+    @EnvironmentObject var dataController: DataController
+    
+    var issues: [Issue] {
+        let filter = dataController.selectedFilter ?? .all
+        var allIssues: [Issue]
+        
+        if let tag = filter.tag {
+            allIssues = tag.issues?.allObjects as? [Issue] ?? []
+        } else {
+            let request = Issue.fetchRequest()
+            request.predicate = NSPredicate(format: "modificationDate > %@", filter.minModificationDate as NSDate)
+            allIssues = (try? dataController.container.viewContext.fetch(request)) ?? []
+        }
+        
+        return allIssues.sorted()
     }
-}
-
-#Preview {
-    ContentView()
+    
+    func delete(_ offsets: IndexSet) {
+        for offset in offsets {
+            let item = issues[offset]
+            dataController.delete(item)
+        }
+    }
+    
+    var body: some View {
+        List {
+            ForEach(issues) { issue in
+                IssueRow(issue: issue)
+            }
+            .onDelete(perform: delete)
+        }
+        .navigationTitle("Issues")
+    }
 }
